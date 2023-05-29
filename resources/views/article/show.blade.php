ce583fb7a51e008c1c621034ab21fbad6cf51e0a
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-05-30 03:03:44 +0630

    blog

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,17 +1,1 @@
-@extends('layouts.app')
-@section('content')
-    <div class=" container">
-        <div class="row">
-            <div class="col-12">
-                <h3>Article Detial</h3>
-                <hr>
-                <h4>
-                    {{ $article->title }}
-                </h4>
-                <div class="">
-                    {{ $article->description }}
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+
